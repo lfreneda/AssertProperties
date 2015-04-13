8ab82f170b139e9a8e049548105b53ed02872e22
Luiz Freneda <lfreneda@gmail.com>
2015-04-13 09:46:59 -0300

    Nuget package restore and first test :o

---
+++ test/AssertProperties.Tests/AssertPropertiesTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AssertProperties.Tests
+{
+    [TestFixture]
+    public class AssertPropertiesTests
+    {
+        public class Foo
+        {
+            public string Name { get; set; }
+        }
+
+        [Test]
+        [ExpectedException(typeof(AssertExpcetion), ExpectedMessage = "\r\nName expected to be NULL but was Luiz\r\n")]
+        public void ShouldBeNull_WhenPropertyIsNotNull_ShouldThrowException()
+        {
+            var foo = new Foo { Name = "Luiz" };
+
+            foo.AssertProperties()
+                    .EnsureThat(f => f.Name).ShouldBeNull()
+                .Assert();
+        }
+    }
+}
